3e09d260ba660fb517486ec4bf76ea5bbfa50b0a
AleixSL <aleixserrallorens@gmail.com>
2021-06-14 18:59:50 +0200

    Canvis a l'api i la web.

--- api/api.py
+++ api/api.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import flask
+from werkzeug.wrappers import request
+from flask_cors import CORS, cross_origin
 import pandas as pd
 import numpy as np
 from tensorflow import keras
@@ -11,19 +13,15 @@ from tensorflow import keras
 # spec.loader.exec_module(script_csv)
 
 app = flask.Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 model_gb = None
 model_no90 = None
 model_pesEndavant = None
 model_torsoInclinat = None
 model_desnivellPeus = None
 
-# El codi serveix per a saber que retorna i poder-ho comunicar al frontend.
-# Codi[0] = good
-# Codi[1] = no90
-# Codi[2] = pesEndavant
-# Codi[3] = torsoInclinat
-# Codi[4] = desnivellPeus
-codi = [0, 0, 0, 0, 0]
+res = []
 
 def load_models():
    global model_gb
@@ -82,34 +80,26 @@ def no90():
     #   Retorna codi no90
     predictions = model_no90.predict_classes(samples)
     # Si 1 a la predicció, llavors vol dir que si que es passen els 90 graus
-    if 1 in predictions:
-        codi[1] = 0
-    else:
-        codi[1] = 1
+    if 0 in predictions:
+        res.append("no90")
 
 def pesEndavant():
     predictions = model_pesEndavant.predict_classes(samples)
     # Si 1 a la predicció, llavors vol dir que no tira el pes endavant
-    if 1 in predictions:
-        codi[2] = 0
-    else:
-        codi[2] = 1
+    if 0 in predictions:
+        res.append("pesEndavant")
 
 def torsoInclinat():
     predictions = model_torsoInclinat.predict_classes(samples)
     # Si 1 a la predicció, llavors vol dir que no inclina el tors
-    if 1 in predictions:
-        codi[3] = 0
-    else:
-        codi[3] = 1
+    if 0 in predictions:
+        res.append("torsoInclinat")
 
 def desnivellPeus():
     predictions = model_desnivellPeus.predict_classes(samples)
     # Si 1 a la predicció, llavors vol dir que no hi ha desnivell de peus
-    if 1 in predictions:
-        codi[4] = 0
-    else:
-        codi[4] = 1
+    if 0 in predictions:
+        res.append("desnivellPeus")
 
 def good_bad():
     # Fem el predict
@@ -120,22 +110,25 @@ def good_bad():
     predictions = model_gb.predict_classes(samples)
     # Cas good
     if 1 in predictions:
-        codi[0] = 1
+        res.append("good")
     # Cas bad
     else:
-        codi[0] = 0
+        res.append("bad")
 
 @app.route("/predict", methods=["POST"])
+@cross_origin()
 def predict():
     global videofile
     global video_path
+
+    #videofile = flask.request.data
     videofile = flask.request.files['videofile']
+
     video_path = "/home/aleix/Escriptori/coses_tfg/tfg-correct-exercise/api/files/" + videofile.filename
     videofile.save(video_path)
-    
-    num = ""
-    global codi
-    codi = [0, 0, 0, 0, 0, 0]
+
+    global res
+    res = []
 
     if not ".avi" in videofile:
         convert_video()
@@ -150,19 +143,11 @@ def predict():
     torsoInclinat()
     desnivellPeus()
 
-    os.system("rm -f ./files/*.avi ./files/*.mp4 ./files/*.csv")
+    os.system("rm -f ./files/*.avi ./files/*.mp4 ./files/*.csv")        
 
-    for i in range(len(codi)):
-        num += str(codi[i])
-
-    data = {"Codi": num }
-    # classification = flask.jsonify(data)
-    classification = data
-    return flask.render_template('index.html', prediction=classification["Codi"])
-
-@app.route("/", methods=["GET"])
-def home_page():
-    return flask.render_template('index.html')
+    data = {"Resultat": res}
+    print(data)
+    return flask.jsonify(data)
 
 if __name__ == "__main__":
     load_models()
--- app/trainer-app/src/app/pujarvideo/pujarvideo.page.ts
+++ app/trainer-app/src/app/pujarvideo/pujarvideo.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
+// import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer/ngx';
+import { File } from '@ionic-native/file';
+
 @Component({
   selector: 'app-pujarvideo',
   templateUrl: './pujarvideo.page.html',
@@ -7,9 +12,91 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PujarvideoPage implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  file: File = null;
+  baseApiUrl = 'http://127.0.0.1:3000/predict'
+  res = null
+  text = ''
+  
+  upload(){
+
+    // Create form data
+    const formData = new FormData();     
+      
+    // Store form name as "file" with file data
+    formData.append("videofile", this.file, this.file.name);
+    console.log(formData.getAll);
+    // Make http post request over api
+    // with formData as req
+    return this.http.post(this.baseApiUrl, formData)
+    .subscribe(data => {
+      console.log(data);
+      this.res = data["Resultat"]
+      console.log(this.res);
+    }, (err) => {
+      console.log(err);
+    });
+
+  }
+
+  tractarCodi() {
+    if(this.res.includes("good") && this.res.length === 1) {
+      this.text = 'Ets el puto amo';
+    }
+  }
 
   ngOnInit() {
   }
 
+  onChange(event) {
+    this.file = event.target.files[0];
+    // console.log(this.file.name)
+  }
+
 }
+
+
+
+
+//   constructor(private http: HttpClient, private transfer: FileTransfer, private file: File) { }
+
+//   fileTransfer: FileTransferObject = this.transfer.create();
+  
+//   // full example
+// upload() {
+//   let options: FileUploadOptions = {
+//      fileKey: 'file',
+//      fileName: 'video.mp4',
+//      headers: {}
+//   }
+
+//   this.fileTransfer.upload('assets/video.mp4', 'http://127.0.0.1:3000/predict', options)
+//    .then((data) => {
+//      // success
+//      console.log(data);
+//    }, (err) => {
+//      // error
+//      console.log(err);
+//    })
+// }
+
+  // runHttp() {
+  //   // this.http.post<any>('http://127.0.0.1:3000/prova2', {Title: "aiushdisauhf"})
+  //   //   .subscribe(data => {
+  //   //     console.log(data);
+  //   //   });
+
+  //   const endpoint = 'http://127.0.0.1:3000/predict';
+  //   const formData: FormData = new FormData();
+  //   this.fileToUpload 
+  //   formData.append('fileKey', 'assets/video.mp4', 'video.mp4');
+  //   this.http.post<any>(endpoint, formData)
+  //     .subscribe(data => {
+  //       console.log(data);
+  //     });
+  //   // this.http.get<any>('http://127.0.0.1:3000/prova')
+  //   //   .subscribe(data => {
+  //   //     console.log(data);
+  //   //   });
+  // }
